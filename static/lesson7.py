9ff22049a620ba71068f0b58dca4edee7b18a4c2
luckylemon8 <133763365+luckylemon8@users.noreply.github.com>
2025-11-17 09:23:19 +0000

    finished lesson 7

---
+++ static/lesson7.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    nameValue = "Bob"
+    timeValue = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return render_template("index.html", name=nameValue, time=timeValue)
+
+
+app.run(debug=True, port=5000)
